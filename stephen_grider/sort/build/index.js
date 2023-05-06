d52e2b642e655d8cc629c88910eb10a60baeccfa
LeonLow97 <lowjiewei@gmail.com>
2023-05-06 21:55:28 +0800

    Added NumbersCollection to sorter

--- stephen_grider/sort/build/index.js
+++ stephen_grider/sort/build/index.js
@@ -1,34 +1,8 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    // bubble sort
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                // All of this only works if collection is number[]
-                // if collection is an array of numbers
-                if (this.collection instanceof Array) { // collection === number[]
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        // swap the elements
-                        const temp = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = temp;
-                    }
-                }
-                // Only going to work if collection is a string
-                // if collection is a string
-                // ~~~logic to compare and swap characters in a string
-                if (typeof this.collection === 'string') {
-                    this.collection.
-                    ;
-                }
-            }
-        }
-    }
-}
-const sorter = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+const sorter_1 = require("./sorter");
+const NumbersCollection_1 = require("./NumbersCollection");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
+const sorter = new sorter_1.Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
