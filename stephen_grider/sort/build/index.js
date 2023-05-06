ab16d1664ee77b9328621ec86a49642dad3691d4
LeonLow97 <lowjiewei@gmail.com>
2023-05-06 22:38:43 +0800

    Added CharactersCollection and LinkedList with interface for sorting

--- stephen_grider/sort/build/index.js
+++ stephen_grider/sort/build/index.js
@@ -1,8 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sorter_1 = require("./sorter");
-const NumbersCollection_1 = require("./NumbersCollection");
-const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
-const sorter = new sorter_1.Sorter(numbersCollection);
+const LinkedListCollection_1 = require("./LinkedListCollection");
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+// const charactersCollection = new CharactersCollection("jeoXijgBeWoigadARbsc")
+// const sorter = new Sorter(charactersCollection)
+// sorter.sort()
+// console.log(charactersCollection.data)
+const linkedListCollection = new LinkedListCollection_1.LinkedListCollection();
+linkedListCollection.add(500);
+linkedListCollection.add(-10);
+linkedListCollection.add(-3);
+linkedListCollection.add(4);
+const sorter = new sorter_1.Sorter(linkedListCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+linkedListCollection.print();
--- stephen_grider/sort/src/index.ts
+++ stephen_grider/sort/src/index.ts
@@ -1,7 +1,24 @@
 import { Sorter } from './sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedListCollection } from './LinkedListCollection';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+// const charactersCollection = new CharactersCollection("jeoXijgBeWoigadARbsc")
+// const sorter = new Sorter(charactersCollection)
+// sorter.sort()
+// console.log(charactersCollection.data)
+
+const linkedListCollection = new LinkedListCollection();
+linkedListCollection.add(500);
+linkedListCollection.add(-10);
+linkedListCollection.add(-3);
+linkedListCollection.add(4);
+
+const sorter = new Sorter(linkedListCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+linkedListCollection.print();
