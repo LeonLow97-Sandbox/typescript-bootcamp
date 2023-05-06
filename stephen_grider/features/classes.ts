5f463a01af202e03f0a13f3228c0082d3e71d65a
LeonLow97 <lowjiewei@gmail.com>
2023-05-06 14:44:25 +0800

    Revision on super keyword

--- stephen_grider/features/classes.ts
+++ stephen_grider/features/classes.ts
@@ -12,6 +12,11 @@ console.log(vehicle.color);
 
 // copy all the methods from Vehicle to Car
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    // refers to immediate parent class object
+    super(color);
+  }
+
   // overriding the drive method of the Parent class
   private drive(): void {
     console.log('vroom vroom');
@@ -23,5 +28,5 @@ class Car extends Vehicle {
   }
 }
 
-const car = new Car('red');
+const car = new Car(4, 'red');
 car.startDrivingProcess();
